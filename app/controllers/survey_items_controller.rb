269110cdfee1eb4a9cc5223dd53e263802ee30e4
Coraline <coraline@idolhands.com>
2026-06-09 00:57:51 -0500

    Works but renders wrong

--- app/controllers/survey_items_controller.rb
+++ app/controllers/survey_items_controller.rb
@@ -3,10 +3,10 @@ class SurveyItemsController < ApplicationController
   def update
     @survey_item = SurveyItem.find(params[:id])
     success = @survey_item.update(survey_item_params)
-    
+
     respond_to do |format|
       format.turbo_stream do
-        render turbo_stream: turbo_stream.replace("survey_item_#{@survey_item.id}", partial: "/survey_items/form", locals: { survey_item: @survey_item, success: success })
+        render turbo_stream: turbo_stream.replace("frame-survey-item-#{@survey_item.id}", partial: "/survey_items/form", locals: { survey_item: @survey_item, success: success })
       end
     end
   end
@@ -14,7 +14,7 @@ class SurveyItemsController < ApplicationController
   private
   
   def survey_item_params
-    params.require(:survey_item).permit(:prompt, :label, :item_kind, :is_ignored)
+    params.require(:survey_item).permit(:prompt, :label, :item_kind, :is_ignored, :dimension)
   end
   
 end
--- app/javascript/schema_editing.js
+++ app/javascript/schema_editing.js
@@ -1,11 +1,7 @@
 export function handleSchemaForms() {
   document.addEventListener("change", (event) => {
-    console.log('ATTACHED')
-    const form = event.target.closest(".survey-item-form")
-    console.log(event.target)
-    console.log(event.target.closest('.radio-row'))
+    const form = event.target.form
     if (!form) return
-    const submitButton = form.querySelector('input[type:"submit"]')
-    submitButton.classList.remove("hidden")
+    form.requestSubmit()
   });
 }

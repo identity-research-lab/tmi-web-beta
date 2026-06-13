b9360dedb51a3f9058094fbbc5505108dfe05f2d
Coraline <coraline@idolhands.com>
2026-06-13 15:38:14 -0500

    Add job in progress bit to project

--- app/models/services/import_from_csv.rb
+++ app/models/services/import_from_csv.rb
@@ -15,23 +15,29 @@ module Services
 			return unless project.participant_id_field.present?
 			return unless project.csv_data.present?
 
-			survey_items = project.active_fields
+			begin
+				project.update_attributes(refresh_in_progress: true)
+				survey_items = project.active_fields
 
-			records = CSV.parse(project.csv_data, headers: true)
-			records.each do |record|
-				next unless persona = Persona.find_or_create_by(participant_id: record[project.participant_id_field])
-				survey_items.each do |survey_item|
-					survey_response = SurveyResponse.find_or_initialize_by(
-						persona: persona,
-						survey_item: survey_item
-					)
-					survey_response.dimension = survey_item.dimension
-					survey_response.value = record[survey_item.csv_header] || "No response"
-					survey_response.save!
+				records = CSV.parse(project.csv_data, headers: true)
+				records.each do |record|
+					next unless persona = Persona.find_or_create_by(participant_id: record[project.participant_id_field])
+					survey_items.each do |survey_item|
+						survey_response = SurveyResponse.find_or_initialize_by(
+							persona: persona,
+							survey_item: survey_item
+						)
+						survey_response.dimension = survey_item.dimension
+						survey_response.value = record[survey_item.csv_header] || "No response"
+						survey_response.save!
+					end
 				end
+				project.update_attributes(refreshed_at: DateTime.now, refresh_in_progress: false)
+				Event.create(project: project, label: "Survey responses", description: "Survey responses import completed, #{records.count} cases refreshed.")
+			rescue Exception
+				project.update_attributes(refresh_in_progress: false)
+				Event.create(project: project, label: "Survey responses", description: "Survey responses import failed.")
 			end
-			project.update_attributes(refreshed_at: DateTime.now)
-      Event.create(project: self, label: "Survey responses", description: "Survey responses import completed, #{records.count} cases refreshed.")
 		end
 
 	end

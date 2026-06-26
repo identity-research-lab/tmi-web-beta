91f30514348b5e5703f18e9278056f6638428fdb
Coraline <coraline@idolhands.com>
2026-06-26 14:22:08 -0500

    Sort direction indicated in UI

--- app/javascript/sort_table.js
+++ app/javascript/sort_table.js
@@ -4,7 +4,25 @@ export function sortTable(columnIndex) {
   if (!table) return
 
   const tbody = table.getElementsByTagName("tbody")[0]
-  var rows = Array.from(tbody.rows)
+  const rows = Array.from(tbody.rows)
+  const theaders = Array.from(table.getElementsByTagName("th"))
+  const sortDirection = theaders[columnIndex].dataset.sortDirection
+
+  theaders.forEach(function(th) {
+    th.classList.remove("sort-asc")
+    th.classList.remove("sort-desc")
+  })
+
+  // toggle
+  if (sortDirection == "desc") {
+    theaders[columnIndex].dataset.sortDirection = "asc"
+    theaders[columnIndex].classList.add("sort-asc")
+    theaders[columnIndex].classList.remove("sort-desc")
+  } else {
+    theaders[columnIndex].dataset.sortDirection = "desc"
+    theaders[columnIndex].classList.add("sort-desc")
+    theaders[columnIndex].classList.remove("sort-asc")
+  }
 
   rows.sort(function(a,b) {
     const valueA = a.cells[columnIndex]?.dataset.sortIndex
@@ -15,9 +33,17 @@ export function sortTable(columnIndex) {
     const isNumericSort = !Number.isNaN(numA) && !Number.isNaN(numB)
 
     if (isNumericSort) {
-      return numB - numA
+      if (sortDirection == "desc") {
+        return numB - numA
+      } else {
+        return numA - numB
+      }
     } else {
-      return valueB.localeCompare(valueA)
+      if (sortDirection == "desc") {
+        return valueB.localeCompare(valueA)
+      } else {
+        return valueA.localeCompare(valueB)
+      }
     }
   }).forEach(tr => tbody.appendChild(tr))
 

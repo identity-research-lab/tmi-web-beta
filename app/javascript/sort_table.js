e9e6515079061e350dfcd87a2dd474e432b06eee
Coraline <coraline@idolhands.com>
2026-06-26 12:59:06 -0500

    Sort text and numbers separately

--- app/javascript/sort_table.js
+++ app/javascript/sort_table.js
@@ -7,9 +7,18 @@ export function sortTable(columnIndex) {
   var rows = Array.from(tbody.rows)
 
   rows.sort(function(a,b) {
-    const valueA = Number(a.cells[columnIndex]?.dataset.sort ?? 0)
-    const valueB = Number(b.cells[columnIndex]?.dataset.sort ?? 0)
-    return valueB - valueA
+    const valueA = a.cells[columnIndex]?.dataset.sortIndex
+    const valueB = b.cells[columnIndex]?.dataset.sortIndex
+
+    const numA = Number(valueA)
+    const numB = Number(valueB)
+    const isNumericSort = !Number.isNaN(numA) && !Number.isNaN(numB)
+
+    if (isNumericSort) {
+      return numB - numA
+    } else {
+      return valueB.localeCompare(valueA)
+    }
   }).forEach(tr => tbody.appendChild(tr))
 
 }

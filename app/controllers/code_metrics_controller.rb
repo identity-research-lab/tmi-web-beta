7acbb1a09ce09af0995d1662c87656a66407972c
Coraline <coraline@idolhands.com>
2026-06-29 18:18:03 -0500

    Save identifiers with survey items

--- app/controllers/code_metrics_controller.rb
+++ app/controllers/code_metrics_controller.rb
@@ -2,15 +2,16 @@ class CodeMetricsController < ApplicationController
   
   def index
     @project = Project.last
-    @survey_items = @project.active_fields.all.sort{|a,b| a.formatted_identifier <=> b.formatted_identifier }
+    @survey_items = SurveyItem.active.order(:identifier)
     @survey_item = params[:survey_item_id] && SurveyItem.find(params[:survey_item_id])
   end
   
   def create
+    @survey_items = SurveyItem.active.order(:identifier)
     @survey_item = params[:survey_item_id] && SurveyItem.find(params[:survey_item_id])
     respond_to do |format|
       format.turbo_stream do
-        render turbo_stream: turbo_stream.replace("survey-item-codes", partial: "/code_metrics/show", locals: { survey_item: @survey_item })
+        render turbo_stream: turbo_stream.replace("survey-item-codes", partial: "/code_metrics/show", locals: { survey_item: @survey_item, survey_items: @survey_items })
       end
     end
   end

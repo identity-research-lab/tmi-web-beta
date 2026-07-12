bb667ffe78fedd569c1846d0e867d2f213c1e3af
Coraline <coraline@idolhands.com>
2026-07-12 15:36:48 -0500

    Enter to add works

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,10 +1,9 @@
 class CodesController < ApplicationController
   
   def create
-    @code = Code.find_or_initialize_by(label: code_params[:label])
     if @survey_response = SurveyResponse.find(code_params[:survey_response])
+      @code = Code.find_or_create_by!(label: code_params[:label], dimension: @survey_response.dimension.name)
       @code.survey_responses << @survey_response unless @code.survey_responses.include? @survey_response
-      @code.save
       respond_to do |format|
         format.turbo_stream do
           render turbo_stream: turbo_stream.replace("codes-for-#{@survey_response.id}", partial: "/codes/show", locals: { survey_response: @survey_response })
@@ -15,9 +14,6 @@ class CodesController < ApplicationController
   
   def update
     Rails.logger.info "Format: #{request.format}"
-    Rails.logger.info "Accept: #{request.headers['Accept']}"
-    Rails.logger.info "Turbo-Frame: #{request.headers['Turbo-Frame']}"
-    Rails.logger.info "Turbo-Visit: #{request.headers['Turbo-Visit']}"
     @code = Code.find(params[:id])
     if @survey_response = SurveyResponse.find(code_params[:survey_response])
       @code.detach_from(@survey_response)

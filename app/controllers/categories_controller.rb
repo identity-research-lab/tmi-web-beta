5190b5ee821e3489c1b2a5955b47d7c7d4d0dead
Coraline <coraline@idolhands.com>
2026-07-15 23:31:05 -0500

    Stats block is live

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,11 @@
 class CategoriesController < ApplicationController
-  
+
   def index
     @project = Project.last
     @categories = Category.all
+    @code_count = Code.count
+    @categorized_code_count = Code.categorized.count
+    @uncategorized_code_count = Code.uncategorized.count
   end
-  
+
 end

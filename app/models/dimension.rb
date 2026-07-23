ce78d363f43ba08f8a7c75c9b6fc89b68948a05c
Coraline <coraline@idolhands.com>
2026-07-22 20:38:48 -0500

    Add researcher to the graph

--- app/models/dimension.rb
+++ app/models/dimension.rb
@@ -1,11 +1,12 @@
 class Dimension
+
   include ActiveGraph::Node
 
   property :name
 
-  has_many :in, :survey_items, type: :HasDimension
-  has_many :in, :survey_responses, type: :HasDimension
-  has_many :in, :codes, type: :HasDimension
-  has_many :in, :reflections, type: :HasDimension
+  has_many :in, :survey_items, type: :RelatesTo
+  has_many :in, :survey_responses, type: :RelatesTo
+  has_many :in, :codes, type: :RelatesTo
+  has_many :in, :reflections, type: :RelatesTo
 
 end

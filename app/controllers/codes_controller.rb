7d7062b8f05f902231b395dcd5d61ee0493ad6cf
Coraline <coraline@idolhands.com>
2026-08-22 00:54:39 -0500

    Themes show

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -24,29 +24,13 @@ class CodesController < ApplicationController
           render turbo_stream: turbo_stream.replace("codes-for-#{survey_response.id}", partial: "/codes/show", locals: { survey_response: survey_response })
         end
       end
-    elsif code_params[:category] && category = Category.find(code_params[:category])
-      if params[:remove]
-        code.categories.delete(category)
-        respond_to do |format|
-          format.turbo_stream do
-            render turbo_stream: turbo_stream.replace("category-codes", partial: "/categories/codes", locals: { category: category, codes: category.codes, attached_codes: category.codes })
-          end
-        end
-      elsif params[:add]
-        code.categories << category unless code.categories.include? category
-        respond_to do |format|
-        format.turbo_stream do
-          render turbo_stream: turbo_stream.replace("category-codes", partial: "/categories/codes", locals: { category: category, codes: category.codes, attached_codes: category.codes })
-        end
-      end
-      end
     end
   end
 
   private
   
   def code_params
-    params.require(:code).permit(:label, :survey_response, :category)
+    params.require(:code).permit(:label, :survey_response)
   end
   
 end

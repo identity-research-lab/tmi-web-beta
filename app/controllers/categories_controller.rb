808d625a275cd77e91cbba10dafa9a7906e7722c
Coraline <coraline@idolhands.com>
2026-09-01 15:20:32 -0500

    Edit category details

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -15,12 +15,25 @@ class CategoriesController < ApplicationController
     redirect_to @category
   end
 
+  def edit
+    @category = Category.find(params[:id])
+    render turbo_stream: turbo_stream.replace("category-header", partial: "/categories/form", locals: { project: @project, category: @category})
+  end
+  
   def show
     @category = Category.find(params[:id])
-    @category_codes = @category.codes
-    @codes = Search.new(query: "*", scope: ["code"], per_page: 50, sort_key: "name").paged_results
-    @memos = @category.memos.order(created_at: :desc)
-    @memo = Memo.new(kind: "category", referrent_id: @category.id)
+    
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace("category-header", partial: "/categories/show", locals: { project: @project, category: @category})
+      end
+      format.html do
+        @category_codes = @category.codes
+        @codes = Search.new(query: "*", scope: ["code"], per_page: 50, limit: 50, sort_key: "name").paged_results
+        @memos = @category.memos.order(created_at: :desc)
+        @memo = Memo.new(kind: "category", referrent_id: @category.id)
+      end
+    end
   end
 
   def destroy
@@ -31,20 +44,30 @@ class CategoriesController < ApplicationController
 
   def update
     @category = Category.find(params[:id])
-    if category_params[:code] && code = Code.find(category_params[:code])
-      if params[:remove]
-        code.categories.delete(@category)
-        respond_to do |format|
-          format.turbo_stream do
-            render turbo_stream: turbo_stream.replace("category-codes-attached", partial: "/categories/codes", locals: { category: @category, codes: @category.codes, attached_codes: @category.codes, pane: "attached" })
-          end
+
+    code = category_params[:code] && Code.find(category_params[:code])
+    if code && params[:remove]
+      code.categories.delete(@category)
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace("category-codes-attached", partial: "/categories/codes", locals: { category: @category, codes: @category.codes, attached_codes: @category.codes, pane: "attached" })
         end
-      elsif params[:add]
-        code.categories << @category unless code.categories.include? @category
-        respond_to do |format|
-          format.turbo_stream do
-            render turbo_stream: turbo_stream.replace("category-codes-attached", partial: "/categories/codes", locals: { category: @category, codes: @category.codes, attached_codes: @category.codes, pane: "attached" })
-          end
+      end
+    end
+    if code && params[:add]
+      code.categories << @category unless code.categories.include? @category
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace("category-codes-attached", partial: "/categories/codes", locals: { category: @category, codes: @category.codes, attached_codes: @category.codes, pane: "attached" })
+        end
+      end
+    end
+
+    if category_params[:name]
+      @category.update_attributes(name: category_params[:name], description: category_params[:description])
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace("category-header", partial: "/categories/show", locals: { category: @category, project: @project })
         end
       end
     end

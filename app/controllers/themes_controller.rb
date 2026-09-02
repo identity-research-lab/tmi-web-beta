6bff6a7d262960ce28e83b366cfd4e9ef575f1a5
Coraline <coraline@idolhands.com>
2026-09-02 18:39:23 -0500

    Checkmarks implemented in theme show

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -16,8 +16,8 @@ class ThemesController < ApplicationController
   end
 
   def edit
-    @category = Category.find(params[:id])
-    render turbo_stream: turbo_stream.replace("category-header", partial: "/categories/form", locals: { project: @project, category: @category})
+    @theme = Theme.find(params[:id])
+    render turbo_stream: turbo_stream.replace("theme-header", partial: "/themes/form", locals: { project: @project, theme: @theme})
   end
   
   def show
@@ -44,23 +44,26 @@ class ThemesController < ApplicationController
 
   def update
     @theme = Theme.find(params[:id])
-    if theme_params[:category] && category = Category.find(theme_params[:category])
-      if params[:remove]
-        @theme.categories.delete(category)
-        respond_to do |format|
-          format.turbo_stream do
-            render turbo_stream: turbo_stream.replace("theme-categories-attached", partial: "/themes/categories", locals: { theme: @theme, categories: @theme.categories, attached_categories: @theme.categories, pane: "attached" })
-          end
-        end
-      elsif params[:add]
-        category.themes << @theme unless category.themes.include? @theme
-        respond_to do |format|
-          format.turbo_stream do
-            render turbo_stream: turbo_stream.replace("theme-categories-attached", partial: "/themes/categories", locals: { theme: @theme, categories: @theme.categories, attached_categories: @theme.categories, pane: "attached" })
-          end
+  
+    category = theme_params[:category] && Category.find(theme_params[:category])
+    if category && params[:remove]
+      category.themes.delete(@theme)
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace("theme-categories-attached", partial: "/themes/categories", locals: { theme: @theme, categories: @theme.categories, attached_categories: @theme.categories, pane: "attached" })
         end
       end
-    elseif theme_params[:name]
+    end
+    if category && params[:add]
+      category.themes << @theme unless category.themes.include? @theme
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace("theme-categories-attached", partial: "/themes/categories", locals: { theme: @theme, categories: @theme.categories, attached_categories: @theme.categories, pane: "attached" })
+        end
+      end
+    end
+    
+    if theme_params[:name]
       @theme.update_attributes(name: theme_params[:name], description: theme_params[:description])
       respond_to do |format|
         format.turbo_stream do
